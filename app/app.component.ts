c3b865ccd8c0033b790e26a947170999e50e466c
Jyothsna <jyothsna@datarpm.com>
2016-12-22 04:21:37 +0530

    service added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+
+@Component ({
+    selector: "my-app",
+    template: `
+        <h1>{{title}}</h1>
+        <my-heroes></my-heroes>
+    `
+  
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+     title = 'Tour of Heroes';
+
+}
